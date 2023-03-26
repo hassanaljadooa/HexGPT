a72eab27e0c6da4db570d62dd28b65c8669340db
Hassan <hello@aljadooa.com>
2023-03-26 14:46:27 -0700

    initial commit

---
+++ src/routes/api/generate/+server.js
@@ -0,0 +1,14 @@
+import { error } from '@sveltejs/kit'
+import {generate_pallet} from '$lib/pallet.js'
+
+// hexgpt.xyz/api/generate?prompt="color theme"
+/** @type {import('./$types').RequestHandler} */
+export async function GET({ url }) {
+    const prompt = url.searchParams.get('prompt')
+
+    console.log(prompt);
+
+    let res = await generate_pallet(prompt)
+
+    return new Response(String(res))
+}
