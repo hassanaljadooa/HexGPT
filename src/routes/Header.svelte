65a9b37aee2b158c5ba44e8d03a6a17208b89008
Hassan <hello@aljadooa.com>
2023-05-09 17:55:11 -0700

    added margin to header button

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,7 +5,7 @@
 	<div class="flex-none">
 		<ul class="menu menu-horizontal px-1">
 			<li><a href="/faq">FAQ</a></li>
-			<li><a href="/about">About</a></li>
+			<li><a class="mr-1" a href="/about">About</a></li>
 			<li><a class="btn btn-outline btn-primary underline-none" href="https://github.com/hassanaljadooa/HexGPT">GitHub<i class="fa-brands fa-github text-lg"></i></a></li>
 		</ul>
 	</div>
