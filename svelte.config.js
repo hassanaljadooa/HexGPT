42d3052d2bf65cc5d7d85bf26610e21f2cd09eaf
Hassan <hello@aljadooa.com>
2023-05-12 14:50:21 -0700

    switched to netlify adapter, cloudflare adapter was having issues.

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-netlify';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
-			}
-		})
+		adapter: adapter()
 	},
 	preprocess: [
 		vitePreprocess(),
